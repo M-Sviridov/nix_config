90f35486dad5827f44d8774040811993093d6ab6
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-02-18 07:07:33 +0100

    Add basic Firefox configuration

--- configuration.nix
+++ configuration.nix
@@ -123,7 +123,46 @@
 
   # List of programs you want to enable
 
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisableFirefoxAccounts = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DNSOverHTTPS = {
+	Fallback = false;
+      };
+      EnableTrackingProtection = {
+	Value = true;
+	Locked = true;
+	Cryptomining = true;
+	Fingerprinting = true;
+      };
+      ExtensionSettings = {
+        # "*".installation_mode = "blocked";
+	"ublock0@raymondhill.net" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+	"{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+	"FirefoxColor@mozilla.com" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/firefox-color/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+      };
+      Homepage = {
+        URL = "https://todo.lan.msviridov.com";
+	StartPage = "homepage";
+      };
+      HttpsOnlyMode = "enabled";
+      OfferToSaveLogins = false;
+      PasswordManagerEnabled = false;
+      PictureInPicture = false;
+    };
+  };
 
   programs.git.enable = true;
 
