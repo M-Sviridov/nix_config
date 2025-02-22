74248ad9262abb465c92aba3444d8735782830c2
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-02-22 11:32:10 +0100

    Add gammastep

--- modules/home/services.nix
+++ modules/home/services.nix
@@ -1,5 +1,18 @@
 {...}: {
   services = {
     mpris-proxy.enable = true;
+    gammastep = {
+      enable = true;
+
+      latitude = 48.40;
+      longitude = 2.70;
+
+      temperature = {
+        day = 6500;
+        night = 2500;
+      };
+
+      provider = "manual";
+    };
   };
 }
