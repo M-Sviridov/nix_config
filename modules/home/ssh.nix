28c52d694297de053afd15223a0cc308dae9523a
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-03-02 10:33:59 +0100

    Add hosts to ssh config

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -8,13 +8,22 @@
       Host *
         User msviridov
         Port 22
+        IdentityFile ~/.ssh/ansible-controller_key
 
       Host github.com
         IdentityFile ~/.ssh/github_key
 
       Host ansible
-        Hostname ansible-controller.tails
-        IdentityFile ~/.ssh/ansible-controller_key
+        Hostname ansible-controller
+
+      Host caddy
+        Hostname caddy
+
+      Host docker-vm01
+        Hostname docker-vm01
+
+      Host podman-vm01
+        Hostname podman-vm01
     '';
   };
 }
