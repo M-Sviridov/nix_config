f2193b12654b34ed60d12eee31410a50c20344ac
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-03-07 13:59:39 +0100

    Reduce scroll step

--- modules/home/wezterm/wezterm.lua
+++ modules/home/wezterm/wezterm.lua
@@ -1,21 +1,35 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 else
-  config = {}
+	config = {}
 end
 
 config.font_size = 12
 
 config.font = wezterm.font({
-  family = "Maple Mono NF",
-  harfbuzz_features = { "calt", "cv01", "ss05", "ss08" },
-  weight = "Regular",
+	family = "Maple Mono NF",
+	harfbuzz_features = { "calt", "cv01", "ss05", "ss08" },
+	weight = "Regular",
 })
 
 config.enable_tab_bar = false
 config.use_fancy_tab_bar = false
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByLine(-3),
+	},
+	{
+		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByLine(3),
+	},
+}
 
 config.window_decorations = "NONE"
 config.window_close_confirmation = "NeverPrompt"
@@ -25,4 +39,3 @@ config.color_scheme = "Catppuccin Mocha"
 config.max_fps = 144
 
 return config
-
