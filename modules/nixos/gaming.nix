420fc22d8fe572191a8fbdec7b391c54099fe897
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-09-13 18:32:03 +0700

    Keep Steam gamescope option only

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -16,20 +16,12 @@
       };
     };
 
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
-
     steam = {
       enable = true;
       gamescopeSession.enable = true;
-      extraCompatPackages = [pkgs.proton-ge-bin];
+      extraCompatPackages = with pkgs; [proton-ge-bin];
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    mangohud
-    protonup
-  ];
+  environment.systemPackages = with pkgs; [mangohud];
 }
