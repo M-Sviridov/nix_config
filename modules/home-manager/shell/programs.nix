740aec8b0ad3b3a88f44661768df295ae11f4ac0
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-09-14 08:55:55 +0700

    Rename since using programs function and not packages

---
+++ modules/home-manager/shell/programs.nix
@@ -0,0 +1,26 @@
+{pkgs, ...}: {
+  programs = {
+    bat = {
+      enable = true;
+      extraPackages = with pkgs.bat-extras; [
+        batman
+        batpipe
+        batgrep
+      ];
+    };
+
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+      colors = "always";
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [
+        "--cmd cd"
+      ];
+    };
+  };
+}
