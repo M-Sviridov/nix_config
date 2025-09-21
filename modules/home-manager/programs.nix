de477eb6a990c943a50c8664a185988131935c01
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-09-21 16:01:45 +0700

    Create desktop entry for Bitwarden to launch with wayland flag

--- modules/home-manager/programs.nix
+++ modules/home-manager/programs.nix
@@ -23,6 +23,19 @@
   xdg = {
     enable = true;
 
+    desktopEntries = {
+      bitwarden = {
+        categories = ["Utility"];
+        comment = "Secure and free password manager for all of your devices";
+        exec = "bitwarden --ozone-platform-hint=auto %U";
+        icon = "bitwarden";
+        genericName = "Password Manager";
+        mimeType = ["x-scheme-handler/bitwarden"];
+        name = "Bitwarden";
+        type = "Application";
+      };
+    };
+
     userDirs = {
       enable = true;
       createDirectories = true;
