4ae8d64655f97c614f816a25031fe8d5313f9731
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-09-21 11:00:19 +0700

    Update git config with optimisations and better defaults

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,11 +1,32 @@
 {...}: {
   programs.git = {
     enable = true;
+
     extraConfig = {
+      branch.sort = "-committerdate";
       color.ui = "auto";
+      column.ui = "auto";
+      commit.verbose = "true";
+      core.compression = 9;
+      diff.algorithm = "histogram";
+      maintenance.strategy = "incremental";
+      merge.conflictStyle = "zdiff3";
       init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      status.branch = true;
+      tag.sort = "-version:refname";
     };
-    delta.enable = true;
+
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        navigate = true;
+        side-by-side = true;
+      };
+    };
+
+    maintenance.enable = true;
     userEmail = "74146348+M-Sviridov@users.noreply.github.com";
     userName = "M-Sviridov";
   };
