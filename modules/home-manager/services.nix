a97704a0847abaaa26da61a216cd8135a337921b
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-09-21 09:28:02 +0700

    Modify vicinae config

--- modules/home-manager/services.nix
+++ modules/home-manager/services.nix
@@ -12,6 +12,19 @@
     vicinae = {
       enable = true;
       autoStart = true;
+      settings = {
+        faviconService = "twenty";
+        font.size = 11;
+        theme.name = "catppuccin-mocha";
+        popToRootOnClose = false;
+        rootSearch.searchFiles = false;
+
+        window = {
+          csd = true;
+          opacity = 0.95;
+          rounding = 10;
+        };
+      };
     };
   };
 }
