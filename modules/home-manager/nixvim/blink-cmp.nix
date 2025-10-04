6df67857c3f07ba7216b51b55fed8bf5d32041bd
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-04 13:20:35 +0700

    Add VSCode like icons

--- modules/home-manager/nixvim/blink-cmp.nix
+++ modules/home-manager/nixvim/blink-cmp.nix
@@ -3,7 +3,36 @@
     enable = true;
 
     settings = {
-      appearance.nerd_font_variant = "mono";
+      appearance = {
+        nerd_font_variant = "mono";
+        kind_icons = {
+          Text = "";
+          Method = "";
+          Function = "";
+          Constructor = "";
+          Field = "";
+          Variable = "";
+          Class = "";
+          Interface = "";
+          Module = "";
+          Property = "";
+          Unit = "";
+          Value = "";
+          Enum = "";
+          Keyword = "";
+          Snippet = "";
+          Color = "";
+          File = "";
+          Reference = "";
+          Folder = "";
+          EnumMember = "";
+          Constant = "";
+          Struct = "";
+          Event = "";
+          Operator = "";
+          TypeParameter = "";
+        };
+      };
 
       completion = {
         documentation.auto_show = false;
