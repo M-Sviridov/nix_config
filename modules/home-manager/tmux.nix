495cc7bb84ea620c804d6d5711a2bc75018e5377
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-04 13:45:00 +0700

    Refactor tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,70 +1,50 @@
 {pkgs, ...}: {
   programs.tmux = {
     enable = true;
-    prefix = "C-a";
+    baseIndex = 1;
+    clock24 = true;
+    disableConfirmationPrompt = true;
+    escapeTime = 0;
+    historyLimit = 10000;
+    mouse = true;
+    sensibleOnTop = true;
+    shortcut = "s";
+    terminal = "tmux-256color";
+
     extraConfig = ''
-      set-option -sa terminal-overrides ",xterm*:Tc"
-      set -g default-terminal "tmux-256color"
+      set -sg terminal-overrides ",*:RGB"
+      set -g default-command "$SHELL"
+      set -g repeat-time 1000
 
-      set -g allow-passthrough on
-
-      set -g status-left-length 100
-      set -g status-left ""
-      set -ga status-left "#{?client_prefix,#{#[bg=#{@thm_red},fg=#{@thm_bg},bold]  #S },#{#[bg=#{@thm_bg},fg=#{@thm_green}]  #S }}"
-      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]│"
-      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_maroon}]  #{pane_current_command} "
-      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]│"
-      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_blue}]  #{=/-32/...:#{s|$USER|~|:#{b:pane_current_path}}} "
-      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]#{?window_zoomed_flag,│,}"
-      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_yellow}]#{?window_zoomed_flag,  zoom ,}"
-
-      set -g status-right-length 100
-      set -g status-right ""
-      set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}, none]"
+      set -g pane-border-lines simple
+      set -g pane-border-style fg=black,bright
+      set -g pane-active-border-style fg=magenta
 
       set -g status-position top
-      set -g status-style "bg=#{@thm_bg}"
-      set -g status-justify "absolute-centre"
+      set -g status-style bg=default,fg=black,bright
+      set -g status-left ""
+      set -g status-right "#[fg=black,bright]#S"
 
-      setw -g pane-border-status top
-      setw -g pane-border-format ""
-      setw -g pane-active-border-style "bg=#{@thm_bg},fg=#{@thm_overlay_0}"
-      setw -g pane-border-style "bg=#{@thm_bg},fg=#{@thm_surface_0}"
-      setw -g pane-border-lines single
-
-      set -wg automatic-rename on
-      set -g automatic-rename-format "Window"
-
-      set -g window-status-format " #I#{?#{!=:#{window_name},Window},: #W,} "
-      set -g window-status-style "bg=#{@thm_bg},fg=#{@thm_rosewater}"
-      set -g window-status-last-style "bg=#{@thm_bg},fg=#{@thm_peach}"
-      set -g window-status-activity-style "bg=#{@thm_red},fg=#{@thm_bg}"
-      set -g window-status-bell-style "bg=#{@thm_red},fg=#{@thm_bg},bold"
-      set -gF window-status-separator "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}]│"
-
-      set -g window-status-current-format " #I#{?#{!=:#{window_name},Window},: #W,} "
-      set -g window-status-current-style "bg=#{@thm_peach},fg=#{@thm_bg},bold"
-
-      set -g @continuum-restore 'on'
-      set -g @resurrect-processes 'ssh'
-      set -g @resurrect-strategy-nvim 'session'
+      set -g window-status-format "●"
+      set -g window-status-current-format "●"
+      set -g window-status-current-style "#{?client_prefix,fg=red,#{?window_zoomed_flag,fg=yellow,fg=magenta},nobold}"
+      set -g window-status-bell-style "fg=red,nobold"
 
       unbind %
       bind | split-window -h
       unbind '"'
       bind - split-window -v
+
+      bind m resize-pane -Z
     '';
-    clock24 = true;
-    mouse = true;
+
     plugins = with pkgs.tmuxPlugins; [
       continuum
-      fzf-tmux-url
+      # fzf-tmux-url
       resurrect
-      sensible
-      tmux-thumbs
+      # tmux-thumbs
       vim-tmux-navigator
-      yank
+      # yank
     ];
-    sensibleOnTop = true;
   };
 }
