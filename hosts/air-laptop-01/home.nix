77d76ebf2d424d8bbb502c5edf23900812bd1b82
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-26 09:40:58 +0700

    Add host specific wezterm settings

---
+++ hosts/air-laptop-01/home.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  outputs,
+  ...
+}: {
+  imports = [
+    outputs.homeManagerModules.catppuccin
+    outputs.homeManagerModules.delta
+    outputs.homeManagerModules.git
+    outputs.homeManagerModules.nixvim
+    outputs.homeManagerModules.shell
+    outputs.homeManagerModules.ssh
+    outputs.homeManagerModules.tmux
+    outputs.homeManagerModules.wezterm
+    outputs.homeManagerModules.zen-browser
+
+    inputs.catppuccin.homeModules.catppuccin
+    inputs.nixvim.homeModules.nixvim
+    inputs.zen-browser.homeModules.twilight-official
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [inputs.nur.overlays.default];
+
+  home = {
+    username = "msviridov";
+    homeDirectory = "/Users/msviridov";
+    stateVersion = "25.05";
+  };
+
+  programs.home-manager.enable = true;
+  news.display = "silent";
+}
--- modules/home-manager/wezterm/wezterm.nix
+++ modules/home-manager/wezterm/wezterm.nix
@@ -1,6 +1,63 @@
-{...}: {
+{hostname, ...}: let
+  machines = {
+    air-laptop-01 = {
+      dpi = 144.0;
+      fontSize = 13.0;
+    };
+
+    loki = {
+      dpi = 384.0;
+      fontSize = 11.0;
+    };
+  };
+
+  settings = machines.${hostname};
+in {
   programs.wezterm = {
     enable = true;
-    extraConfig = builtins.readFile ./wezterm.lua;
+    extraConfig = ''
+      local wezterm = require 'wezterm'
+      local act = wezterm.action
+
+      if wezterm.config_builder then
+        config = wezterm.config_builder()
+      else
+        config = {}
+      end
+
+      config.font_size = ${toString settings.fontSize}
+      config.dpi = ${toString settings.dpi}
+
+      config.font = wezterm.font {
+        family = 'Maple Mono NF',
+        harfbuzz_features = { 'calt', 'cv01', 'ss05', 'ss08' },
+        weight = 'Regular',
+      }
+
+      config.enable_tab_bar = false
+      config.use_fancy_tab_bar = false
+
+      config.mouse_bindings = {
+        {
+          event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+          mods = 'NONE',
+          action = act.ScrollByLine(-3),
+        },
+        {
+          event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+          mods = 'NONE',
+          action = act.ScrollByLine(3),
+        },
+      }
+
+      config.window_decorations = 'RESIZE'
+      config.window_close_confirmation = 'NeverPrompt'
+
+      config.color_scheme = 'Catppuccin Mocha'
+
+      config.max_fps = 144
+
+      return config
+    '';
   };
 }
