20c6706e0255911dc86cf5e23fdff3fc7d387633
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-26 10:17:49 +0700

    Enable Go lsp

--- modules/home-manager/nixvim/lsp.nix
+++ modules/home-manager/nixvim/lsp.nix
@@ -6,6 +6,7 @@
       servers = {
         bashls.enable = true;
         docker_language_server.enable = true;
+        gopls.enable = true;
         lua_ls.enable = true;
         markdown_oxide.enable = true;
         nixd.enable = true;
