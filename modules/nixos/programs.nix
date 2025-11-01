82a629e8903680e5dd5614e502d3fa63593d8c3b
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-01 09:09:46 +0700

    Rename config to nix_config to be as repo name on github

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -11,7 +11,7 @@
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 7d --keep 2";
-      flake = "/home/msviridov/dotfiles-nix";
+      flake = "/home/msviridov/nix_config";
     };
 
     vivid = {
