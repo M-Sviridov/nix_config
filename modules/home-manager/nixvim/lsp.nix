b460d45fe494f43ba21a8f9099c04139d26ad316
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-03 08:37:18 +0700

    Enable and configure gopls LSP

--- modules/home-manager/nixvim/lsp.nix
+++ modules/home-manager/nixvim/lsp.nix
@@ -7,7 +7,26 @@
         ansiblels.enable = true;
         bashls.enable = true;
         docker_language_server.enable = true;
-        gopls.enable = true;
+
+        gopls = {
+          enable = true;
+          config = {
+            settings = {
+              gopls = {
+                "ui.inlayhint.hints" = {
+                  assignVariableTypes = true;
+                  compositeLiteralFields = true;
+                  compositeLiteralTypes = true;
+                  constantValues = true;
+                  functionTypeParameters = true;
+                  parameterNames = true;
+                  rangeVariableTypes = true;
+                };
+              };
+            };
+          };
+        };
+
         lua_ls.enable = true;
         markdown_oxide.enable = true;
         nixd.enable = true;
