f3fbac36dd68d3dcce295b3ec39eddc208730b3e
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-01-07 09:50:50 +0700

    Disable extra UWSM config

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -11,14 +11,14 @@
       withUWSM = true;
     };
 
-    uwsm = {
-      waylandCompositors = {
-        hyprland = {
-          prettyName = "Hyprland";
-          comment = "Hyprland compositor managed by UWSM";
-          binPath = "/run/current-system/sw/bin/Hyprland";
-        };
-      };
-    };
+    # uwsm = {
+    #   waylandCompositors = {
+    #     hyprland = {
+    #       prettyName = "Hyprland";
+    #       comment = "Hyprland compositor managed by UWSM";
+    #       binPath = "/run/current-system/sw/bin/start-hyprland";
+    #     };
+    #   };
+    # };
   };
 }
