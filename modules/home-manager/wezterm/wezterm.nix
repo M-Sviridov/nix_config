f7dd27bfc019ab459dbc194f3a1e63949d21ee7e
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-01-19 13:08:08 +0700

    Transform wezterm module to be a custom module with sensible defaults

--- modules/home-manager/wezterm/wezterm.nix
+++ modules/home-manager/wezterm/wezterm.nix
@@ -1,67 +1,121 @@
-{hostname, ...}: let
-  machines = {
-    air-laptop-01 = {
-      dpi = 144.0;
-      fontSize = 14.0;
-      windowDecorations = "RESIZE";
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.my.programs.wezterm;
+in {
+  options.my.programs.wezterm = {
+    enable = mkEnableOption "WezTerm terminal emulator";
+
+    fontSize = mkOption {
+      type = types.float;
+      default = 12.0;
+      description = "Font size";
     };
 
-    loki = {
-      dpi = 384.0;
-      fontSize = 11.0;
-      windowDecorations = "NONE";
+    dpi = mkOption {
+      type = types.float;
+      default = 96.0;
+      description = "Display DPI for font scaling";
+    };
+
+    windowDecorations = mkOption {
+      type = types.enum ["FULL" "RESIZE" "NONE" "TITLE"];
+      default = "FULL";
+      description = "Window decoration style";
+    };
+
+    font = {
+      family = mkOption {
+        type = types.str;
+        default = "Maple Mono NF";
+        description = "Font family";
+      };
+
+      features = mkOption {
+        type = types.listOf types.str;
+        default = ["calt" "cv01" "ss05" "ss08"];
+        description = "HarfBuzz font features";
+      };
+
+      weight = mkOption {
+        type = types.str;
+        default = "Regular";
+        description = "Font weight";
+      };
+    };
+
+    colorScheme = mkOption {
+      type = types.str;
+      default = "Catppuccin Mocha";
+      description = "Color scheme name";
+    };
+
+    maxFps = mkOption {
+      type = types.int;
+      default = 144;
+      description = "Maximum frames per second";
+    };
+
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = "Additional Lua configuration appended before return";
     };
   };
 
-  settings = machines.${hostname};
-in {
-  programs.wezterm = {
-    enable = true;
-    extraConfig = ''
-      local wezterm = require 'wezterm'
-      local act = wezterm.action
+  config = mkIf cfg.enable {
+    programs.wezterm = {
+      enable = true;
+      extraConfig = ''
+        local wezterm = require 'wezterm'
+        local act = wezterm.action
 
-      if wezterm.config_builder then
-        config = wezterm.config_builder()
-      else
-        config = {}
-      end
+        if wezterm.config_builder then
+          config = wezterm.config_builder()
+        else
+          config = {}
+        end
 
-      -- Host specific settings
-      config.font_size = ${toString settings.fontSize}
-      config.dpi = ${toString settings.dpi}
-      config.window_decorations = "${settings.windowDecorations}"
+        -- Host specific settings
+        config.font_size = ${toString cfg.fontSize}
+        config.dpi = ${toString cfg.dpi}
+        config.window_decorations = "${cfg.windowDecorations}"
 
-      -- Shared settings
-      config.font = wezterm.font {
-        family = 'Maple Mono NF',
-        harfbuzz_features = { 'calt', 'cv01', 'ss05', 'ss08' },
-        weight = 'Regular',
-      }
+        -- Font settings
+        config.font = wezterm.font {
+          family = '${cfg.font.family}',
+          harfbuzz_features = { ${concatMapStringsSep ", " (f: "'${f}'") cfg.font.features} },
+          weight = '${cfg.font.weight}',
+        }
 
-      config.enable_tab_bar = false
-      config.use_fancy_tab_bar = false
+        -- Shared settings
+        config.enable_tab_bar = false
+        config.use_fancy_tab_bar = false
 
-      config.mouse_bindings = {
-        {
-          event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-          mods = 'NONE',
-          action = act.ScrollByLine(-3),
-        },
-        {
-          event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-          mods = 'NONE',
-          action = act.ScrollByLine(3),
-        },
-      }
+        config.mouse_bindings = {
+          {
+            event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+            mods = 'NONE',
+            action = act.ScrollByLine(-3),
+          },
+          {
+            event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+            mods = 'NONE',
+            action = act.ScrollByLine(3),
+          },
+        }
 
-      config.window_close_confirmation = 'NeverPrompt'
+        config.window_close_confirmation = 'NeverPrompt'
+        config.color_scheme = '${cfg.colorScheme}'
+        config.max_fps = ${toString cfg.maxFps}
 
-      config.color_scheme = 'Catppuccin Mocha'
+        ${cfg.extraConfig}
 
-      config.max_fps = 144
-
-      return config
-    '';
+        return config
+      '';
+    };
   };
 }
