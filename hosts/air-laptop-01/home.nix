f82657792468218f5bc10c3b5f745b59d1ac77bd
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-01-19 13:08:28 +0700

    Enable and configure custom my.* modules

--- hosts/air-laptop-01/home.nix
+++ hosts/air-laptop-01/home.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   outputs,
   ...
@@ -22,7 +23,39 @@
     inputs.zen-browser.homeModules.twilight-official
   ];
 
-  packages.common.enable = true;
+  my = {
+    packages.common.enable = true;
+
+    programs = {
+      git = {
+        enable = true;
+        userName = "M-Sviridov";
+        userEmail = "74146348+M-Sviridov@users.noreply.github.com";
+        signing.key = "${config.home.homeDirectory}/.ssh/sign_github_ed25519.pub";
+
+        extraSettings = {
+          pull.rebase = true;
+          rerere.enabled = true;
+        };
+      };
+
+      wezterm = {
+        enable = true;
+        fontSize = 14.0;
+        dpi = 144.0;
+        windowDecorations = "RESIZE";
+      };
+    };
+
+    shell.zsh = {
+      enable = true;
+      sshAuthSock = "/Users/msviridov/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock";
+      rebuildCommand = "darwin-rebuild switch --flake .#air-laptop-01";
+      upgradeCommand = "nix flake update && darwin-rebuild switch --flake .#air-laptop-01";
+      cleanCommand = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
+      homeRebuildCommand = "home-manager switch --flake .#msviridov@air-laptop-01";
+    };
+  };
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [inputs.nur.overlays.default];
