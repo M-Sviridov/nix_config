1656699011513c7ea7d31e93ab7ad636b94a3d54
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-02-12 08:34:16 +0700

    Add determinate-nix options

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -14,10 +14,12 @@
       substituters = [
         "https://hyprland.cachix.org"
         "https://vicinae.cachix.org"
+        "https://install.determinate.systems"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       ];
     };
   };
