c1299917015f2c2404b1ce3134165da557bacc41
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-03-27 17:20:01 +0700

    Update ghostty module

--- modules/aspects/ghostty.nix
+++ modules/aspects/ghostty.nix
@@ -2,8 +2,9 @@
   my.ghostty.homeManager = {
     programs.ghostty = {
       enable = true;
+      enableZshIntegration = true;
       settings = {
-        font-family = "Maple Mono NF";
+        font-family = ["Maple Mono NF" "Noto Sans Thai"];
         font-feature = ["calt" "cv01" "ss05" "ss08"];
         font-size = 12;
       };
