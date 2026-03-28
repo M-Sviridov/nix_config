d7a50f0ec8794af73b6b36f24a510e2a55b65c46
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-03-28 16:28:22 +0700

    Remove gnome and xserver

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -45,12 +45,8 @@
         LC_TIME = "th_TH.UTF-8";
       };
 
-      # Enable the X11 windowing system.
-      services.xserver.enable = true;
-
       # Enable the GNOME Desktop Environment.
       services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome.enable = true;
 
       # Configure keymap in X11
       services.xserver.xkb = {
