f2c73daa97cec6d0274b8dd6a05536fa7ff0ee01
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-03-30 08:40:08 +0700

    Add zen-browser module

---
+++ modules/aspects/zen-browser/bookmarks.nix
@@ -0,0 +1,213 @@
+{
+  my.zen-browser.homeManager = {
+    programs.zen-browser = {
+      policies = {
+        Bookmarks = [
+          # ENTERTAINMENT
+          {
+            Title = "Jellyfin";
+            URL = "https://jellyfin.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Entertainment";
+          }
+          {
+            Title = "Jellyseerr";
+            URL = "https://jellyseerr.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Entertainment";
+          }
+          {
+            Title = "Reddit";
+            URL = "https://reddit.com";
+            Placement = "toolbar";
+            Folder = "Entertainment";
+          }
+          {
+            Title = "YouTube";
+            URL = "https://youtube.com";
+            Placement = "toolbar";
+            Folder = "Entertainment";
+          }
+
+          # HOMELAB
+          {
+            Title = "AdGuard Home";
+            URL = "https://adguard.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Coder";
+            URL = "https://coder.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Dozzle";
+            URL = "https://dozzle.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "FreshRSS";
+            URL = "https://rss.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Frigate";
+            URL = "https://frigate.lan.leclubavon.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "HAOS";
+            URL = "https://haos.lan.leclubavon.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Heimdall";
+            URL = "https://heimdall.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Immich";
+            URL = "https://immich.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Ntfy";
+            URL = "https://ntfy.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Open WebUI";
+            URL = "https://openwebui.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "OPNsense";
+            URL = "https://opnsense.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "qBittorrent";
+            URL = "https://qbit.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Radarr";
+            URL = "https://radarr.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Radarr Anime";
+            URL = "https://radarr-anime.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Sonarr";
+            URL = "https://sonarr.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Sonarr Anime";
+            URL = "https://sonarr-anime.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "Speedtest";
+            URL = "https://speedtest.lan.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+          {
+            Title = "UniFi Site Manager";
+            URL = "https://unifi.ui.com/";
+            Placement = "toolbar";
+            Folder = "Homelab";
+          }
+
+          # PERSO
+          {
+            Title = "Coros Training Hub";
+            URL = "https://t.coros.com/admin/views/dash-board";
+            Placement = "toolbar";
+            Folder = "Perso";
+          }
+          {
+            Title = "Proton Mail";
+            URL = "https://mail.proton.me";
+            Placement = "toolbar";
+            Folder = "Perso";
+          }
+          {
+            Title = "Proton Pass";
+            URL = "https://proton.me/pass";
+            Placement = "toolbar";
+            Folder = "Perso";
+          }
+          {
+            Title = "Tailscale";
+            URL = "https://login.tailscale.com/admin/machines";
+            Placement = "toolbar";
+            Folder = "Perso";
+          }
+          {
+            Title = "VaultWarden";
+            URL = "https://vault.msviridov.com";
+            Placement = "toolbar";
+            Folder = "Perso";
+          }
+
+          # PRO
+          {
+            Title = "Gmail";
+            URL = "https://mail.google.com";
+            Placement = "toolbar";
+            Folder = "Pro";
+          }
+          {
+            Title = "Google Calendar";
+            URL = "https://calendar.google.com";
+            Placement = "toolbar";
+            Folder = "Pro";
+          }
+
+          # TECH
+          {
+            Title = "MyNixOS";
+            URL = "https://mynixos.com/";
+            Placement = "toolbar";
+            Folder = "Tech";
+          }
+          {
+            Title = "NixOS Wiki";
+            URL = "https://wiki.nixos.org/wiki/NixOS_Wiki";
+            Placement = "toolbar";
+            Folder = "Tech";
+          }
+
+          # TRACKERS
+          {
+            Title = "RED";
+            URL = "https://redacted.sh";
+            Placement = "toolbar";
+            Folder = "Trackers";
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ modules/aspects/zen-browser/zen-browser.nix
@@ -0,0 +1,216 @@
+{
+  den,
+  inputs,
+  ...
+}: {
+  flake-file.inputs = {
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
+
+  my.zen-browser.homeManager = {
+    config,
+    pkgs,
+    ...
+  }: {
+    imports = [inputs.zen-browser.homeModules.twilight];
+    nixpkgs.overlays = [inputs.nur.overlays.default];
+
+    home.file = {
+      ".config/zen/default/chrome/userChrome.css" = {
+        source = ./userChrome.css;
+      };
+      ".config/zen/default/chrome/userContent.css" = {
+        source = ./userContent.css;
+      };
+      ".config/zen/default/chrome/zen-logo-mocha.svg" = {
+        source = ./zen-logo-mocha.svg;
+      };
+    };
+
+    programs.zen-browser = {
+      enable = true;
+      nativeMessagingHosts = [pkgs.firefoxpwa];
+
+      languagePacks = [
+        "en-US"
+        "fr"
+        "ru"
+        "th"
+      ];
+
+      policies = {
+        AppAutoUpdate = false;
+        AutofillAddressEnabled = false;
+        AutofillCreditCardEnabled = false;
+
+        Cookies = {
+          Allow = [
+            "https://auth.msviridov.com/"
+            "https://chatgpt.com/"
+            "https://claude.ai/"
+            "https://github.com/"
+            "https://google.com/"
+            "https://jellyfin.lan.msviridov.com/"
+            "https://jellyseerr.msviridov.com/"
+            "https://mistral.ai/"
+            "https://ntfy.lan.msviridov.com/"
+            "https://proton.me/"
+            "https://reddit.com/"
+            "https://soundcloud.com/"
+            "https://t.coros.com/admin/views/dash-board"
+            "https://ui.com/"
+            "https://web.whatsapp.com/"
+            "https://youtube.com/"
+          ];
+
+          Behavior = "reject-tracker-and-partition-foreign";
+        };
+
+        DefaultDownloadDirectory = "${config.xdg.userDirs.download}";
+        DisableFirefoxAccounts = true;
+        DisableFirefoxStudies = true;
+        DisableFormHistory = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisplayMenuBar = "never";
+
+        EnableTrackingProtection = {
+          Value = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+
+        FirefoxSuggest = {
+          ImproveSuggest = false;
+          SponsoredSuggestions = false;
+          WebSuggestions = false;
+        };
+
+        HardwareAcceleration = true;
+        HttpsOnlyMode = "enabled";
+        MoreFromMozilla = false;
+        NetworkPrediction = false;
+        OfferToSaveLogins = false;
+        PasswordManagerEnabled = false;
+
+        PictureInPicture = {
+          Enabled = true;
+        };
+
+        PrintingEnabled = true;
+        PromptForDownloadLocation = false;
+        RequestedLocales = ["en-US" "fr" "th" "ru"];
+
+        SanitizeOnShutdown = {
+          Cookies = true;
+          FormData = true;
+          History = true;
+        };
+
+        SearchSuggestEnabled = false;
+        ShowHomeButton = false;
+      };
+
+      profiles."default" = {
+        id = 0;
+        isDefault = true;
+        containersForce = true;
+        spacesForce = true;
+
+        containers = {
+          "Personal" = {
+            color = "purple";
+            icon = "circle";
+            id = 1;
+          };
+
+          "Work" = {
+            color = "blue";
+            icon = "briefcase";
+            id = 2;
+          };
+        };
+
+        spaces = let
+          containers = config.programs.zen-browser.profiles."default".containers;
+        in {
+          "Personal" = {
+            container = containers."Personal".id;
+            id = "1198a8cf-c49d-481f-a0d6-487f16c90220";
+            icon = "💻";
+            position = 1000;
+          };
+
+          "Work" = {
+            container = containers."Work".id;
+            id = "30e5feb9-eb6b-4c59-8588-d1e9a6b777e3";
+            icon = "💼";
+            position = 2000;
+          };
+        };
+
+        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+          bitwarden
+          catppuccin-web-file-icons
+          karakeep
+          multi-account-containers
+          proton-pass
+          skip-redirect
+          sponsorblock
+          stylus
+          ublock-origin
+          vimium
+          yomitan
+        ];
+
+        search = {
+          default = "ddg";
+          engines = {
+            "amazondotcom-us".metaData.hidden = true;
+            "bing".metaData.hidden = true;
+            "ebay".metaData.hidden = true;
+            "google".metaData.hidden = true;
+            "wikipedia".metaData.hidden = true;
+          };
+
+          force = true;
+          order = ["ddg"];
+          privateDefault = "ddg";
+        };
+
+        settings = {
+          ## GENERAL ##
+          "browser.preferences.defaultPerformanceSettings.enabled" = false;
+
+          ## UI RESPONSIVENES ##
+          "ui.submenuDelay" = 0;
+          "dom.element.animate.enabled" = true;
+          "mousewheel.min_line_scroll_amount" = 15;
+          "mousewheel.scroll_series_timeout" = 10;
+
+          ## MEDIA & CODECS ##
+          "dom.media.webcodecs.h265.enabled" = true;
+          "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled" = true;
+          "media.ffmpeg.vaapi.enabled" = true;
+          "media.hardware-video-decoding.force-enabled" = true;
+
+          ## LINUX SPECIFIC ##
+          "widget.wayland.opaque-region.enabled" = true;
+          "widget.wayland.fractional-scale.enabled" = true;
+
+          ## ZEN SPECIFIC & AI TOOLS ##
+          "reader.parse-on-load.enabled" = false;
+          "zen.urlbar.behavior" = "float";
+          "zen.welcome-screen.seen" = true;
+        };
+      };
+    };
+  };
+}
