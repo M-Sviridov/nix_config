904434509fb09a6c4f7a1469add2f30257625d62
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-04-19 10:15:08 +0700

    Add gaming module

---
+++ modules/aspects/gaming.nix
@@ -0,0 +1,26 @@
+{
+  my.gaming.nixos = {
+    programs = {
+      corectrl = {
+        enable = true;
+        gpuOverclock.enable = true;
+      };
+
+      gamemode.enable = true;
+      gamescope.enable = true;
+
+      steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = false;
+        gamescopeSession.enable = true;
+      };
+    };
+  };
+
+  my.gaming.homeManager = {pkgs, ...}: {
+    home.packages = [pkgs.protonup-qt];
+
+    programs.mangohud.enable = true;
+  };
+}
