8b2e861f7fbf604fc96b26f716b9a45ea6a7b710
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-04-28 14:37:38 +0700

    Enable mpv, rbw for bitwarden and ripgrep programs

--- modules/aspects/programs.nix
+++ modules/aspects/programs.nix
@@ -21,6 +21,38 @@
         enableZshIntegration = true;
       };
 
+      mpv = {
+        enable = true;
+
+        config = {
+          hwdec = "auto";
+          profile = "default";
+          save-position-on-quit = "yes";
+          sub-auto = "fuzzy";
+          sub-border-size = 1;
+          sub-font = "Noto Sans";
+          sub-shadow = 3;
+          sub-shadow-color = "#000000";
+          sub-shadow-offset = 2;
+        };
+
+        scripts = with pkgs.mpvScripts; [
+          mpris
+        ];
+      };
+
+      rbw = {
+        enable = true;
+
+        settings = {
+          base_url = "https://vault.msviridov.com";
+          email = "perso@msviridov.com";
+          pinentry = pkgs.pinentry-gnome3;
+        };
+      };
+
+      ripgrep.enable = true;
+
       zoxide = {
         enable = true;
         enableZshIntegration = true;
