f995b08e1208d0b6ea7bc2e315b70d0d1d7b48e6
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-04-28 14:36:19 +0700

    Update dms config

--- modules/aspects/niri/dms.nix
+++ modules/aspects/niri/dms.nix
@@ -1,41 +1,77 @@
-{
-  den,
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   flake-file.inputs = {
+    danksearch = {
+      url = "github:AvengeMedia/danksearch";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     dms = {
       url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    dms-plugin-registry = {
+      url = "github:AvengeMedia/dms-plugin-registry";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  my.niri.homeManager = {config, ...}: {
+  my.niri.nixos = {
+    programs.kdeconnect.enable = true;
+  };
+
+  my.niri.homeManager = {pkgs, ...}: {
     imports = [
+      inputs.danksearch.homeModules.dsearch
       inputs.dms.homeModules.dank-material-shell
       inputs.dms.homeModules.niri
+      inputs.dms-plugin-registry.modules.default
     ];
 
-    programs.dank-material-shell = {
-      enable = true;
-      niri = {
-        enableSpawn = true;
-        includes = {
-          enable = true;
-          override = true;
-          originalFileName = "hm";
-          filesToInclude = [
-            "alttab"
-            "binds"
-            "colors"
-            "cursor"
-            "layout"
-            "outputs"
-            "windowrules"
-            "wpblur"
-          ];
+    home.packages = [
+      pkgs.libqalculate
+      pkgs.sshfs
+    ];
+
+    programs = {
+      dank-material-shell = {
+        enable = true;
+        enableSystemMonitoring = true;
+        enableVPN = true;
+        enableAudioWavelength = true;
+        enableCalendarEvents = true;
+        enableClipboardPaste = true;
+
+        niri = {
+          enableSpawn = true;
+          includes = {
+            enable = true;
+            override = true;
+            originalFileName = "hm";
+            filesToInclude = [
+              "alttab"
+              "binds"
+              "colors"
+              "cursor"
+              "layout"
+              "outputs"
+              "windowrules"
+              "wpblur"
+            ];
+          };
+        };
+
+        plugins = {
+          calculator.enable = true;
+          dankBatteryAlerts.enable = true;
+          dankBitwarden.enable = true;
+          dankGifSearch.enable = true;
+          dankKDEConnect.enable = true;
+          emojiLauncher.enable = true;
         };
       };
+
+      dsearch.enable = true;
     };
+
+    services.kdeconnect.enable = true;
   };
 }
