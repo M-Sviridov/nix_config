873c3d1978f50079676e9f4cdba7bbc523e1ca08
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-05-09 18:21:04 +0700

    Configure gamemode and automatic keyd disabling while gaming

--- modules/aspects/gaming.nix
+++ modules/aspects/gaming.nix
@@ -1,8 +1,29 @@
 {
-  my.gaming.nixos = {
+  my.gaming.nixos = {pkgs, ...}: {
     programs = {
       corectrl.enable = true;
-      gamemode.enable = true;
+
+      gamemode = {
+        enable = true;
+        settings = {
+          general = {
+            renice = 10;
+            ioprio = 0;
+          };
+
+          custom = {
+            start = "${pkgs.systemd}/bin/systemctl stop keyd";
+            end = "${pkgs.systemd}/bin/systemctl start keyd";
+          };
+
+          gpu = {
+            amd_performance_level = "high";
+            apply_gpu_optimisations = "accept-responsibility";
+            gpu_device = 1;
+          };
+        };
+      };
+
       gamescope.enable = true;
 
       steam = {
