1bf805d50163be2aac47064e9a74694e8b91cc77
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-05-18 19:17:03 +0200

    Change time zone

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -28,7 +28,7 @@
       networking.networkmanager.enable = true;
 
       # Set your time zone.
-      time.timeZone = "Asia/Bangkok";
+      time.timeZone = "Europe/Paris";
 
       # Select internationalisation properties.
       i18n.defaultLocale = "en_US.UTF-8";
