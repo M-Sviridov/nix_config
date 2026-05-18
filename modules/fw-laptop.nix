8ef4b7e1ea09cadc0fa67c5045fc54a7025f54cf
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-05-18 19:18:05 +0200

    Enable printing services and airplay capabilities to pipewire

--- modules/fw-laptop.nix
+++ modules/fw-laptop.nix
@@ -2,9 +2,36 @@
   den.aspects.fw-laptop = {
     nixos = {pkgs, ...}: {
       services = {
+        avahi = {
+          enable = true;
+          nssmdns4 = true;
+          openFirewall = true;
+        };
         gnome.sushi.enable = true;
         gvfs.enable = true;
         upower.enable = true;
+
+        pipewire = {
+          # opens UDP ports 6001-6002
+          raopOpenFirewall = true;
+
+          extraConfig.pipewire = {
+            "10-airplay" = {
+              "context.modules" = [
+                {
+                  name = "libpipewire-module-raop-discover";
+
+                  # increase the buffer size if you get dropouts/glitches
+                  # args = {
+                  #   "raop.latency.ms" = 500;
+                  # };
+                }
+              ];
+            };
+          };
+        };
+
+        printing.enable = true;
       };
 
       environment.systemPackages = with pkgs; [
