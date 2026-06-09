f25e7b08858fc85cafcc723a40d593fa775b1162
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2026-06-09 11:14:21 +0200

    Add and remove packages, and set default applications handler

--- modules/msviridov.nix
+++ modules/msviridov.nix
@@ -33,27 +33,108 @@
       ];
 
     homeManager = {pkgs, ...}: {
-      nixpkgs.config.allowUnfree = true;
+      nixpkgs = {
+        config = {
+          allowUnfree = true;
+          permittedInsecurePackages = [
+            "electron-39.8.10"
+          ];
+        };
+      };
 
       home.packages = with pkgs; [
         anki
         ansible
+        awscli2
         bitwarden-desktop
+        czkawka
+        dnsutils
+        doggo
         ente-auth
+        evince
+        feishin
+        ffmpeg-full
+        fragments
+        gnome-calendar
         jq
-        logseq
+        libreoffice
         loupe
+        nmap
         obsidian
+        opentofu
         plezy
         proton-vpn
+        qbittorrent
         signal-desktop
+        traceroute
+        usbutils
       ];
 
-      xdg.mimeApps = {
-        enable = true;
-        defaultApplications = {
-          "inode/directory" = "org.gnome.Nautilus.desktop";
-          "x-scheme-handler/file" = "org.gnome.Nautilus.desktop";
+      xdg = {
+        mimeApps = {
+          enable = true;
+          defaultApplications = {
+            # --- File Manager ---
+            "inode/directory" = "org.gnome.Nautilus.desktop";
+
+            # --- Web / URLs ---
+            "x-scheme-handler/http" = "zen-twilight.desktop";
+            "x-scheme-handler/https" = "zen-twilight.desktop";
+            "x-scheme-handler/ftp" = "zen-twilight.desktop";
+            "x-scheme-handler/chrome" = "zen-twilight.desktop";
+            "text/html" = "zen-twilight.desktop";
+            "application/xhtml+xml" = "zen-twilight.desktop";
+
+            # --- Email ---
+            # "x-scheme-handler/mailto" = "thunderbird.desktop";
+            # "message/rfc822" = "thunderbird.desktop";
+
+            # --- PDF / Documents ---
+            "application/pdf" = "org.pwmt.zathura.desktop";
+            "application/epub+zip" = "org.pwmt.zathura.desktop";
+            "application/x-cbz" = "org.pwmt.zathura.desktop";
+
+            # --- Office Documents ---
+            "application/msword" = "writer.desktop";
+            "application/vnd.oasis.opendocument.text" = "writer.desktop";
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "writer.desktop";
+            "application/vnd.ms-excel" = "calc.desktop";
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "calc.desktop";
+            "application/vnd.ms-powerpoint" = "impress.desktop";
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation" = "impress.desktop";
+
+            # --- Images ---
+            "image/jpeg" = "imv-dir.desktop";
+            "image/png" = "imv-dir.desktop";
+            "image/gif" = "imv-dir.desktop";
+            "image/webp" = "imv-dir.desktop";
+            "image/svg+xml" = "imv-dir.desktop";
+            "image/tiff" = "imv-dir.desktop";
+            "image/bmp" = "imv-dir.desktop";
+            "image/avif" = "imv-dir.desktop";
+
+            # --- Video ---
+            "video/mp4" = "mpv.desktop";
+            "video/webm" = "mpv.desktop";
+            "video/x-matroska" = "mpv.desktop";
+            "video/quicktime" = "mpv.desktop";
+            "video/x-msvideo" = "mpv.desktop";
+            "video/mpeg" = "mpv.desktop";
+            "video/ogg" = "mpv.desktop";
+
+            # --- Torrents ---
+            "application/x-bittorrent" = "fragments.desktop";
+            "x-scheme-handler/magnet" = "fragments.desktop";
+
+            # --- Calendar / Contacts ---
+            # "text/calendar" = "thunderbird.desktop"; # or gnome-calendar
+            # "text/x-vcard" = "thunderbird.desktop";
+
+            # --- Misc Scheme Handlers ---
+            "x-scheme-handler/about" = "zen-twilight.desktop";
+            "x-scheme-handler/unknown" = "zen-twilight.desktop";
+            "x-scheme-handler/terminal" = "com.mitchellh.ghostty.desktop";
+          };
         };
       };
     };
